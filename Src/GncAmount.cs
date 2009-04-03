1a124289a27b6928ba293a2d00cf98d1056e4f7a
rstarkov <sourcecode@roman.st>
2009-04-03 07:41:53 +0100

    Oops forgotten file. (imported from Perforce changelist 806)

---
+++ Src/GncAmount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RT.Util;
+
+namespace GnuCashSharp
+{
+    /// <summary>
+    /// Represents an amount of a certain currency/commodity at a certain point in time.
+    /// </summary>
+    public class GncAmount
+    {
+        private decimal _quantity;
+        private GncCommodity _commodity;
+        private DateTime _timepoint;
+
+        public GncAmount(decimal quantity, GncCommodity commodity, DateTime timepoint)
+        {
+            _quantity = quantity;
+            _commodity = commodity;
+            _timepoint = timepoint;
+            if (_timepoint.Kind != DateTimeKind.Utc)
+                throw new RTException("The DateTime passed to GncAmount constructor must be a UTC DateTime.");
+        }
+
+        /// <summary>
+        /// Gets the quantity of the <see cref="Commodity"/> represented by this instance.
+        /// </summary>
+        public decimal Quantity
+        {
+            get { return _quantity; }
+        }
+
+        /// <summary>
+        /// Gets the commodity that the amount is specified in.
+        /// </summary>
+        public GncCommodity Commodity
+        {
+            get { return _commodity; }
+        }
+
+        /// <summary>
+        /// Gets the point in time at which the amount is defined. The time is always in UTC.
+        /// </summary>
+        public DateTime Timepoint
+        {
+            get { return _timepoint; }
+        }
+
+        /// <summary>
+        /// Converts this amount to a different commodity at the same point in time.
+        /// </summary>
+        public GncAmount ConvertTo(GncCommodity toCommodity)
+        {
+            decimal fromRate = _commodity.IsBaseCurrency ? 1m : _commodity.ExRate.Get(_timepoint, GncInterpolation.Linear);
+            decimal toRate = toCommodity.IsBaseCurrency ? 1m : toCommodity.ExRate.Get(_timepoint, GncInterpolation.Linear);
+            return new GncAmount(_quantity * fromRate / toRate, toCommodity, _timepoint);
+        }
+
+        /// <summary>
+        /// Converts this amount to an amount of the same commodity at a different point in time,
+        /// taking present-value effects into account.
+        /// <param name="rate">The rate of appreciation, p.a.</param>
+        /// </summary>
+        public GncAmount ConvertTo(DateTime presentTime, double rate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
